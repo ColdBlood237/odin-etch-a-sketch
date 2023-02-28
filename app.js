4591bfe113f3e56f5d180496de1875d0ef2befea
ColdBlood237 <115800548+ColdBlood237@users.noreply.github.com>
2023-02-28 15:00:20 +0100

    Add two buttons to change and reset the layout

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const grid = document.getElementById("grid");
+const changeLayoutButton = document.getElementById("change-layout");
+const resetButton = document.getElementById("reset-button");
 
 
-function makeRows(rows, cols) {
+makeGrid(16, 16);
+draws();
+
+
+changeLayoutButton.addEventListener("click", () => {
+    const sideValue = prompt("Enter the number of squares per side (max 100): ");
+    if (sideValue > 1 && sideValue <= 100) {
+        removeGrid();
+        makeGrid(sideValue, sideValue);
+        draws();
+    }
+});
+
+resetButton.addEventListener("click", resetGrid);
+
+function makeGrid(rows, cols) {
     grid.style.setProperty('--grid-rows', rows);
     grid.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
@@ -11,12 +28,30 @@ function makeRows(rows, cols) {
     };
 };
 
-makeRows(16, 16);
+function removeGrid() {
+    const cells = document.querySelectorAll(".grid-item");
+    cells.forEach(cell => {
+        cell.remove();
+    })
+};
 
-const cells = document.querySelectorAll(".grid-item");
-
-cells.forEach(element => {
-    element.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = "black";
+function resetGrid() {
+    const cells = document.querySelectorAll(".grid-item");
+    console.log(cells);
+    cells.forEach(element => {
+        element.style.backgroundColor = "white";
     });
-});
+}
+
+function draws() {
+    const cells = document.querySelectorAll(".grid-item");
+    console.log(cells);
+    cells.forEach(element => {
+        element.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = "black";
+        });
+    });
+}
+
+
+
